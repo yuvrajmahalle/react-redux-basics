075dde8a3cf3b8b9dfa9a77e087f749598a8f102
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-01-16 13:24:18 +0530

    improve code resuability and leaner code using utility function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility'
 
 const initialState = {
     counter : 0,
@@ -6,20 +7,17 @@ const initialState = {
     
 }
 
+const deleteResult = (state, action) =>{
+    const updatedArray = state.results.filter(result => result.id !== action.resultIdStr);
+    return updateObject(state,{results :updatedArray});
+}
+
 const reducer = (state = initialState,action) =>{
     switch(action.type){
         case actionTypes.STORE_VALUE:
-            return{
-                ...state,
-                results:state.results.concat({id: new Date(), value: action.result})
-            }
+            return updateObject(state,{results:state.results.concat({id: new Date(), value: action.result})});
         case actionTypes.DELETE_VALUE:
-            const updatedArray = state.results.filter(result => result.id !== action.resultIdStr);
-        return{
-            ...state,
-            results: updatedArray
-        }
-
+            return deleteResult(state,action);
     }
     return state;
 }
